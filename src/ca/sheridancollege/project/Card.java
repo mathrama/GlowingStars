9b394706f0d60c739661e7469b4dce87d2c118c7
sin10244 <59933748+sin10244@users.noreply.github.com>
2020-01-30 16:37:49 -0500

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -2,7 +2,6 @@
  * SYST 17796 Project Winter 2020 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
- * Hardeep Singh
  */
 package ca.sheridancollege.project;
 
@@ -11,6 +10,9 @@ package ca.sheridancollege.project;
  * enough to be instantiated for any Card game. Students wishing to add to the code 
  * should remember to add themselves as a modifier.
  * @author Sivagama
+ */
+/**
+ * Team Name: Glowing Stars.
  */
 public abstract class Card 
 {
